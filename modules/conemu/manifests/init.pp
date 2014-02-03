fcc14be539dd063608ee7b8d60f815d0b03ccf24
Robin Lee Powell <rlpowell@digitalkingdom.org>
2014-02-02 22:19:35 -0800

    Whitespace.

--- modules/conemu/manifests/init.pp
+++ modules/conemu/manifests/init.pp
@@ -1,13 +1,13 @@
 class conemu {
-	package { 'ConEmu':
-		ensure => installed,
-		       provider => 'chocolatey',
-	}
+  package { 'ConEmu':
+    ensure => installed,
+    provider => 'chocolatey',
+  }
 
-	file { "$homepath/Desktop/ConEmu.lnk":
-		require => Package['ConEmu'],
-			source => "$wapath/modules/conemu/files/conemu.lnk",
-	}
+  file { "$homepath/Desktop/ConEmu.lnk":
+    require => Package['ConEmu'],
+    source => "$wapath/modules/conemu/files/conemu.lnk",
+  }
 
-	windows_pin_startmenu { "$homepath/Desktop/ConEmu.lnk": }
+  windows_pin_startmenu { "$homepath/Desktop/ConEmu.lnk": }
 }
