e569c37f34609ab3e9fdda523dc2bd04b1ed2ddb
Robin Lee Powell <rlpowell@digitalkingdom.org>
2014-02-02 23:44:14 -0800

    Use the new registry loader.

--- modules/pdfxchange/manifests/init.pp
+++ modules/pdfxchange/manifests/init.pp
@@ -9,16 +9,10 @@ class pdfxchange {
     provider => chocolatey,
   }
 
-  $viewer_reg = regsubst("\"$secretspath/pdfxchange_viewer.reg\"", '/', '\\', 'G')
-  $lite_reg = regsubst("\"$secretspath/pdfxchange_lite.reg\"", '/', '\\', 'G')
-
-  exec { 'pdfxchange viewer reg':
-    command => "$cmd /c regedit /s $viewer_reg",
+  windows_extras::regload { "$secretspath/pdfxchange_viewer.reg":
     require => Package['PDFXChangeViewer'],
   }
-
-  exec { 'pdfxchange lite reg':
-    command => "$cmd /c regedit /s $lite_reg",
-    require => Package['pdfxchangelite'],
+  windows_extras::regload { "$secretspath/pdfxchange_lite.reg":
+    require => Package['PDFXChangeViewer'],
   }
 }
