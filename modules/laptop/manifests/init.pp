f4cc6e02b868b8c3f7ef7825c5da6516fd6c68a1
Robin Lee Powell <rlpowell@digitalkingdom.org>
2014-02-02 23:42:45 -0800

    Separating out the laptop specific stuff.

---
+++ modules/laptop/manifests/init.pp
@@ -0,0 +1,8 @@
+class laptop {
+  windows_extras::regload { "$wapath/extras/keyboard.reg": }
+
+  $high_perf_power = regsubst("\"$wapath/extras/high_perf.pow\"", '/', '\\', 'G')
+  exec { 'high performance power settinsg':
+    command => "$cmd /c powercfg -S 381b4222-f694-41f0-9685-ff5bb260df2e & $cmd /c powercfg -D 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c & $cmd /c powercfg -import $high_perf_power 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c & cmd /c powercfg -S 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c",
+  }
+}
