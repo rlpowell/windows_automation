089397237547a902311bc53bd359bd357e50ce4f
Robin Lee Powell <rlpowell@digitalkingdom.org>
2014-02-04 01:35:30 -0800

    Path of exile config files.

---
+++ modules/path_of_exile/manifests/init.pp
@@ -0,0 +1,23 @@
+class path_of_exile {
+#**************
+# Path Of Exile
+#**************
+  package { 'pathofexile':
+    ensure => installed,
+    provider => chocolatey,
+  }
+  windows_pin_startmenu { "$env_programdata/Microsoft/Windows/Start Menu/Programs/Grinding Gear Games/Path of Exile.lnk": }
+
+  if $laptop {
+    file { "$homepath/Documents/My Games/Path of Exile/production_Config.ini":
+      ensure => "$wapath/extras/poe_laptop.ini",
+      force => true,
+    }
+  }
+  if $desktop {
+    file { "$homepath/Documents/My Games/Path of Exile/production_Config.ini":
+      ensure => "$wapath/extras/poe_desktop.ini",
+      force => true,
+    }
+  }
+}
