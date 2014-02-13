ee0c59a3211642ff06beaaa6ca4cd731232d4408
Robin Lee Powell <rlpowell@digitalkingdom.org>
2014-02-13 00:40:53 -0800

    Viewer doesn't seem to work well anymore.

--- modules/pdfxchange/manifests/init.pp
+++ modules/pdfxchange/manifests/init.pp
@@ -4,19 +4,25 @@ class pdfxchange {
 #**************
   # Don't have an Editor license yet
   # package { [ 'pdfxchangelite', 'PDFXChangeViewer', 'PDFXchangeEditor' ]:
-  package { [ 'pdfxchangelite', 'PDFXChangeViewer' ]:
+  #package { [ 'pdfxchangelite', 'PDFXChangeViewer' ]:
+  package { [ 'PDFXchangeEditor', 'pdfxchangelite' ]:
     ensure => installed,
     provider => chocolatey,
   }
 
-  windows_extras::regload { "$secretspath/pdfxchange_viewer.reg":
-    require => Package['PDFXChangeViewer'],
-    unless_key => 'HKEY_CURRENT_USER\Software\Tracker Software\PDFViewer\Registration',
+  #windows_extras::regload { "$secretspath/pdfxchange_viewer.reg":
+  #  require => Package['PDFXChangeViewer'],
+  #  unless_key => 'HKEY_CURRENT_USER\Software\Tracker Software\PDFViewer\Registration',
+  #  unless_check  => 'rlpowell@digitalkingdom.org',
+  #}
+  windows_extras::regload { "$secretspath/pdfxchange_lite.reg":
+    require => Package['pdfxchangelite'],
+    unless_key => 'HKEY_CURRENT_USER\Software\Tracker Software\PDF-XChange Lite 5',
     unless_check  => 'rlpowell@digitalkingdom.org',
   }
-  windows_extras::regload { "$secretspath/pdfxchange_lite.reg":
-    require => Package['PDFXChangeViewer'],
-    unless_key => 'HKEY_CURRENT_USER\Software\Tracker Software\PDF-XChange Lite 5',
+  windows_extras::regload { "$secretspath/pdfxchange_editor.reg":
+    require => Package['PDFXchangeEditor'],
+    unless_key => 'HKEY_CURRENT_USER\Software\Tracker Software\PDFXEditor\3.0\Settings\Registration',
     unless_check  => 'rlpowell@digitalkingdom.org',
   }
 }
