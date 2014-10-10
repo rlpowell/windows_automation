5e18e8dea898a51586eee8c6f7dcd9ae66749620
Robin Lee Powell <rlpowell@digitalkingdom.org>
2014-10-10 00:31:29 -0700

    Separate icons yay.

--- modules/firefox/manifests/init.pp
+++ modules/firefox/manifests/init.pp
@@ -34,4 +34,33 @@ class firefox {
   }
 
   windows_pin_taskbar { "$homepath/Desktop/FF Profiles.lnk": }
+
+  exec { "second FF":
+    command => "$cmd /c xcopy \"C:\\Program Files (x86)\\Mozilla Firefox\" \"C:\\Program Files (x86)\\Mozilla Firefox Home\\\" /s/h/e/k/f/c/o/y",
+    require => Exec["hard link firefox profiles"],
+    creates => 'C:\Program Files (x86)\Mozilla Firefox Home',
+  }
+
+  file { "$homepath/Desktop/FF Home.lnk":
+    source => "$wapath/modules/firefox/files/FF Home.lnk",
+  }
+
+  windows_pin_taskbar { "$homepath/Desktop/FF Home.lnk":
+    require => File["$homepath/Desktop/FF Home.lnk"],
+  }
+
+  exec { "third FF":
+    command => "$cmd /c xcopy \"C:\\Program Files (x86)\\Mozilla Firefox\" \"C:\\Program Files (x86)\\Mozilla Firefox CB\\\" /s/h/e/k/f/c/o/y",
+    require => Exec["hard link firefox profiles"],
+    creates => 'C:\Program Files (x86)\Mozilla Firefox CB',
+  }
+
+  file { "$homepath/Desktop/FF CB.lnk":
+    source => "$wapath/modules/firefox/files/FF CB.lnk",
+  }
+
+  windows_pin_taskbar { "$homepath/Desktop/FF CB.lnk":
+    require => File["$homepath/Desktop/FF CB.lnk"],
+  }
+
 }
