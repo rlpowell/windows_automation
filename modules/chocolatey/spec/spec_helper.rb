72ef1b781dc9a56f103489280403b5d830808db8
Robin Lee Powell <rlpowell@digitalkingdom.org>
2015-12-16 00:16:00 -0800

    Got the latest chocolatey from puppetforge.

--- modules/chocolatey/spec/spec_helper.rb
+++ modules/chocolatey/spec/spec_helper.rb
@@ -1,14 +1,18 @@
+#require 'ruby-prof'
+#RubyProf.start
+
 IDEAL_CONSOLE_WIDTH = 72
 def horizontal_rule(width = 5)
   '=' * [width, IDEAL_CONSOLE_WIDTH].min
 end
 
+require 'puppetlabs_spec_helper/module_spec_helper'
+
 # require dependencies
 gems = [
-  'minitest/autorun', # http://docs.seattlerb.org/minitest/
-  'minitest/unit', # https://github.com/freerange/mocha#bundler
-  'mocha/setup', # http://gofreerange.com/mocha/docs/Mocha/Configuration.html
-  'jumanjiman_spec_helper',
+  #'minitest/autorun', # http://docs.seattlerb.org/minitest/
+  #'minitest/unit', # https://github.com/freerange/mocha#bundler
+  'mocha', # http://gofreerange.com/mocha/docs/Mocha/Configuration.html
   'puppet',
 ]
 begin
@@ -24,12 +28,29 @@ rescue => e
 end
 
 RSpec.configure do |c|
-  # https://github.com/jumanjiman/jumanjiman_spec_helper#shared-contexts
-  c.include JumanjimanSpecHelper::EnvironmentContext
+  # set the environment variable before files are loaded, otherwise it is too late
+  ENV['ChocolateyInstall'] = 'c:\blah'
 
   # https://www.relishapp.com/rspec/rspec-core/v/2-12/docs/mock-framework-integration/mock-with-mocha!
   c.mock_framework = :mocha
-
   # see output for all failures
   c.fail_fast = false
+  c.expect_with :rspec do |e|
+    e.syntax = [:should, :expect]
+  end
+  c.raise_errors_for_deprecations!
+
+  c.after :suite do
+    #result = RubyProf.stop
+    # Print a flat profile to text
+    #printer = RubyProf::FlatPrinter.new(result)
+    #printer.print(STDOUT)
+  end
+end
+
+# We need this because the RAL uses 'should' as a method.  This
+# allows us the same behaviour but with a different method name.
+class Object
+  alias :must :should
+  alias :must_not :should_not
 end
