0fa1d9a30ff6ac84f535bc47498a6e72f24c04bd
Manu Hernandez <manu@littlemousetrap.com>
2017-01-24 10:13:34 +0100

    New version and URL

--- chocolatey/anki/tools/chocolateyInstall.ps1
+++ chocolatey/anki/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'anki' 'exe' '/S' 'http://ankisrs.net/download/mirror/anki-2.0.36.exe'
+Install-ChocolateyPackage 'anki' 'exe' '/S' 'https://apps.ankiweb.net/downloads/current/anki-2.0.38.exe'
