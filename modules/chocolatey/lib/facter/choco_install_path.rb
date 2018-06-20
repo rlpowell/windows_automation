94bf443b132a663c2e1efde5520f96b0de6ad99d
Robin Lee Powell <rlpowell@digitalkingdom.org>
2018-06-19 17:32:43 -0700

    Moving to latest Chocolatey module.

--- modules/chocolatey/lib/facter/choco_install_path.rb
+++
@@ -1,8 +0,0 @@
-require 'pathname'
-require Pathname.new(__FILE__).dirname + '../' + 'puppet_x/chocolatey/chocolatey_install'
-
-Facter.add('choco_install_path') do
-  setcode do
-    PuppetX::Chocolatey::ChocolateyInstall.install_path
-  end
-end
