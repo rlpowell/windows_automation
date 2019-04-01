f9c93f21c16b94e0cce5197f25e067812c352c32
Robin Lee Powell <rlpowell@digitalkingdom.org>
2019-03-31 22:30:05 -0700

    Wanted to see more detail.

--- bin/puppet_apply.bat
+++ bin/puppet_apply.bat
@@ -4,5 +4,7 @@ call "C:\Program Files\Puppet Labs\Puppet\bin\environment.bat" %0 %*
 
 REM Display Ruby version
 ruby.exe -v
+REM Display Puppet version
+echo "" | puppet --version
 
 echo "" | puppet apply --test --verbose --modulepath "C:\Users\rlpowell\Dropbox\Windows_Automation\modules" "C:\Users\rlpowell\Dropbox\Windows_Automation\site.pp"
