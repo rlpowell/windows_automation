ac84557515ba3c03d41949765f7d913027140799
Robin Lee Powell <rlpowell@digitalkingdom.org>
2022-06-30 19:19:30 -0700

    Added a value to the registry check

--- modules/misc/manifests/init.pp
+++ modules/misc/manifests/init.pp
@@ -43,7 +43,8 @@ class misc( $laptop, $desktop ) {
 #**************
   windows_extras::regload { "$wapath/extras/language.reg":
     unless_key => 'HKEY_CURRENT_USER\Control Panel\International',
-    unless_check  => 'yyyy-MM-dd',
+    unless_value => 'sShortDate',
+    unless_check => 'yyyy-MM-dd',
   }
 
 #**************
@@ -276,7 +277,7 @@ service { 'WSearch':
       unless_key => 'HKEY_CURRENT_USER\Keyboard Layout\Substitutes',
       unless_check => '00000409',
     }
-  
+
     $high_perf_power = regsubst("\"$wapath/extras/high_perf_laptop.pow\"", '/', '\\', 'G')
     exec { 'high performance power settings':
       command => "$cmd /c powercfg -S 381b4222-f694-41f0-9685-ff5bb260df2e & $cmd /c powercfg -D 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c & $cmd /c powercfg -import $high_perf_power 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c & cmd /c powercfg -S 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c",
